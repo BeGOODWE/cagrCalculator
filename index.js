f66cb1708694b1e08e607e0dd5bd91880800dd38
navaithani <navaithanii@gmail.com>
2022-06-04 21:34:01 +0100

    add old price feature

--- index.js
+++ index.js
@@ -19,10 +19,20 @@ $(document).ready(function(){
     var price0 = $(".fvOld").val();
     var t = $(".fvYears").val();
 
-    console.log(cagr, price0, t);
-
     var price1 = ((cagr + 1)**t)*price0
     price1 = Math.round(price1*100) / 100
     $(".fvResult").text("New price is: " + price1);
+  });
+});
+
+$(document).ready(function(){
+  $(".pvButton").click(function(){
+    var cagr = $(".pvCAGR").val() / 100;
+    var price1 = $(".pvNew").val();
+    var t = $(".pvYears").val();
+
+    var price0 = price1 / ((cagr + 1)**t)
+    price0 = Math.round(price0*100) / 100
+    $(".pvResult").text("Old price is: " + price0);
   });
 });
