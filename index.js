f0a5fbcd3b1a06221ced48de8e9607681aa653c8
navaithani <navaithanii@gmail.com>
2022-06-04 21:06:59 +0100

    add new price feature

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $("button").click(function(){
+  $(".cagrButton").click(function(){
     var price1 = $(".new").val();
     var price0 = $(".old").val();
     var t = $(".years").val();
@@ -10,5 +10,19 @@ $(document).ready(function(){
 
 
     $(".result").text("CAGR is: " + cagrPercentage + "%");
+  });
+});
+
+$(document).ready(function(){
+  $(".fvButton").click(function(){
+    var cagr = $(".fvCAGR").val() / 100;
+    var price0 = $(".fvOld").val();
+    var t = $(".fvYears").val();
+
+    console.log(cagr, price0, t);
+
+    var price1 = ((cagr + 1)**t)*price0
+    price1 = Math.round(price1*100) / 100
+    $(".fvResult").text("New price is: " + price1);
   });
 });
